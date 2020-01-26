607605884287325a2ff8280db6c7634c55113189
Kit Choi <kit@kychoi.org>
2020-01-26 20:54:36 +0000

    More reorganization

--- many-to-many/postgres/setup.py
+++
@@ -1,5 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="many_to_many",
-)
--- postgres/many-to-many/run.sh
+++ postgres/many-to-many/run.sh
@@ -8,7 +8,9 @@ PGPASSWORD=postgres
 echo $PGPASSWORD
 docker stop $NAME && docker container rm $NAME
 docker run --name $NAME -p $PORT:5432 -d postgres:$TAG
-sleep 5
+sleep 10
 echo "Running at port: $PORT"
+psql -h 0.0.0.0 -p $PORT -U postgres -a -f script.sql
+
 read -n 1 -p "Continue?"
 docker stop $NAME && docker container rm $NAME
